98aa34462473394401145ad16d3eb053f2d9d340
sagardmni <sagardmni@gmail.com>
2016-11-13 18:05:19 -0500

    Do basic redirect to recommendations page through button

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -15,20 +15,21 @@ mysql.init_app(app)
 
 @app.route('/')
 def main():
-  #tuple
   movies = list(users())
   movie_titles = [str(i[0]) for i in movies]
-  print(movie_titles)
   return render_template('index.html', movies = movie_titles)
 
 def users():
   cur = mysql.connection.cursor()
-  #cur.execute('''SELECT * FROM movie limit 10''')
   cur.execute('''SELECT title from movie as table1 inner join 
                 (SELECT movie_id, COUNT(*) AS magnitude  FROM UserRating  GROUP BY movie_id  ORDER BY magnitude DESC LIMIT 10) 
                 as table2 ON (table1.movie_id = table2.movie_id)''')
   movies = cur.fetchall()
   return movies
+
+@app.route('/recommendations', methods=['POST'])
+def recommendations():
+  return render_template('recommendations.html')
 
 if __name__ == "__main__":
   app.run()
